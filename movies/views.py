69f952f85abce69b1a23abac1327d00885646ab7
Anton Dvornikov <antonpostrest@gmail.com>
2020-02-25 21:15:33 +0300

    View movies list on hp added

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
 # Create your views here.
+from django.views.generic.base import View
+
+from .models import Movie
+
+class MoviesView(View):
+    """Movies list"""
+    def get(self, request):
+        movies = Movie.objects.all()
+        return render(request, "movies/movies.html", {"movie_list": movies})
